f64d2dc469df55582cc853799ccdbffd1d201be7
Michael Marcel <michaelmarcel.kz@protonmail.com>
2023-09-22 19:56:41 -0300

    refactor: typed return makeAuthenticateService

--- src/services/factories/makeAuthenticateService.ts
+++ src/services/factories/makeAuthenticateService.ts
@@ -1,7 +1,7 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prismaUsersRepository'
 import { AuthenticateService } from '../authenticateService'
 
-export function makeAuthenticateService() {
+export function makeAuthenticateService(): AuthenticateService {
     const usersRepository = new PrismaUsersRepository()
     const authenticateService = new AuthenticateService(usersRepository)
 
