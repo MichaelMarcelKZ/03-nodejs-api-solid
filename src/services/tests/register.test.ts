c0388cb1660a46fbac1f4066b3ac663e49c3f93d
Michael Marcel <michaelmarcel.kz@protonmail.com>
2023-09-22 12:38:57 -0300

    test: should be able to register

--- src/services/tests/register.test.ts
+++ src/services/tests/register.test.ts
@@ -7,7 +7,21 @@ import { UserAlreadyExitsError } from '../errors/userAlreadyExistsError'
 
 describe('Register Service', () => {
 
-    it('Hashed user password', async () => {
+    it('should be able to register', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const registerService = new RegisterService(usersRepository)
+
+        const password = 'test@123'
+
+        const { user } = await registerService.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password,
+        })
+        expect(user.id).toEqual(expect.any(String))
+    })
+
+    it('should hash user password upon registration', async () => {
         const usersRepository = new InMemoryUsersRepository()
         const registerService = new RegisterService(usersRepository)
 
