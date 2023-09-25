879872485aaf09ccd36b36f773b29fb3001f5a39
Michael Marcel <michaelmarcel.kz@protonmail.com>
2023-09-24 21:22:07 -0300

    feat: refresh token controller test: refresh token controller created

--- src/controllers/users/routes.ts
+++ src/controllers/users/routes.ts
@@ -1,12 +1,17 @@
 import { FastifyInstance } from 'fastify'
+
 import { registerController } from './registerController'
 import { authenticateController } from './authenticateController'
 import { getUserProfileController } from './getUserProfileController'
+import { refreshTokenController } from './refreshTokenController'
+
 import { ensureAuthenticated } from '@/middlewares/ensureAuthenticated'
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/users', registerController)
     app.post('/sessions', authenticateController)
+
+    app.patch('/token/refresh', refreshTokenController)
 
     /** Authenticated */
     app.get('/profile', { onRequest: [ensureAuthenticated] }, getUserProfileController)
